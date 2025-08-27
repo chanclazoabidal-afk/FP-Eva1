2160e11c0e975c54514ecf7a3dc4c2f2c6345ded
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-08-26 17:03:56 -0700

    Add files via upload

---
+++ eva1_1_hola_mundo;/src/eva1_1_hola_mundo/Eva1_1_hola_mundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author Erick
+ */
+public class Eva1_1_hola_mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUDNO");
+        System.out.println("FUNDAMENTOS DE INVESTIGACION");
+    }
+    
+}
