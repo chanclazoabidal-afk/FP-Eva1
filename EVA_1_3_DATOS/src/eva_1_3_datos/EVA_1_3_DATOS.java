a84488eadbbc2a2ea70810c3f4f76db3290b51b2
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-08-27 22:26:57 -0700

    Add files via upload

---
+++ EVA_1_3_DATOS/src/eva_1_3_datos/EVA_1_3_DATOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_3_datos;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_3_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("NOMBRE DEL ESTUDIANTE:");
+        System.out.println("Erick De La Rosa:");
+                System.out.print("NUMERO DE CONTROL:");
+                        System.out.println("2550591:");
+                                System.out.print("Edad:");
+                                        System.out.println("18:");
+    }
+    
+}
