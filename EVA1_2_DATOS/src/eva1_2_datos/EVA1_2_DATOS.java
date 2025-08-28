76c93926982fe406dcb2a2718dd89725a2661a09
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-08-27 21:56:31 -0700

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+// TODO code application logic here
+//; SIGNIFICA FIN DE LINEA
+//"    "  CADENA DE TEXTO. DATO
+        System.out.println("RUBEN HERNANDEZ");
+        //NUMEROS ENTEROS
+        System.out.println(1000);
+        //NUMEROS DECIMALES
+        System.out.println(500.3);
+        //VALORES LOGICOS (VERDADERO FALSO)
+        //TRUE FALSE
+        System.out.println(true);
+        //CARACTERES INDIVIDUALES
+        //COSAS DISTINTAS
+        System.out.println("C");//CARACTER
+        System.out.println("C");//CADENA DE TEXTO
+    }
+    
+}
