ffb77f3acdc15c2e1a441ca3a54fcc39452c4c65
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-08-27 22:28:08 -0700

    Add files via upload

---
+++ EVA1_4_EMPLEO/src/eva1_4_empleo/EVA1_4_EMPLEO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_empleo;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA1_4_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        System.out.print("EMPLEO:");
+        System.out.println("ANALIZIS DE DATOS:");
+                System.out.print("SALARIO:");
+                        System.out.println("25,000:");
+                                System.out.print("HORAS A LA SEMANA:");
+                                        System.out.println("30 HORAS/SEMANA:");
+                                        System.out.print("ENGLISH REQUIEREMENT:");
+                                        System.out.println("true:");
+    }
+    
+}
