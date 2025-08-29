645b535940a9816105ae70775e7f2b88e3dd95f4
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-08-28 22:11:22 -0700

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declarar variables
+        //tipo de dato             nombre del identificador
+        
+        String nombre;
+        int edad;
+        double salario;
+        //INICIALIZACION:
+        //IDENTIFICADOR = VALOR
+        //= --> OPERADOR DE ASIGNACION
+        nombre = "Erick Eduardo De La Rosa Nunex";
+        edad=43;
+        salario= 1.1;
+        //edad="erick"
+        System.out.println(nombre);
+         System.out.println(edad);
+          System.out.println(salario);
+        
+        
+        
+    }
+    
+}
