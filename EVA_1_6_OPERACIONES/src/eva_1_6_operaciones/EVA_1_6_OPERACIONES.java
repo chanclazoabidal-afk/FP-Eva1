63db15e451f3eca7c661c85ec89d54e7f1bbc282
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-09-01 21:44:30 -0700

    Add files via upload

---
+++ EVA_1_6_OPERACIONES/src/eva_1_6_operaciones/EVA_1_6_OPERACIONES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_6_operaciones;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    double pi, radio, area;
+    //ES LO MISMO QUE;
+    //double pi;
+    //double radio;
+    //double area;
+    //inicializacion:
+    pi = 3.1416;
+    radio = 10;
+    //OPERACION;
+    area = pi * radio * radio;
+    //SALIDA DE DATOS;
+    System.out.println(" El area de un circulo de radio 10 es");
+    System.out.println(area);
+    }
+    
+}
+    
