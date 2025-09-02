41c8e048a70bbf9bafa996784744a251c59cf935
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-09-01 21:55:41 -0700

    Add files via upload

---
+++ EVA_1_7_TRIANGULO/src/eva_1_7_triangulo/EVA_1_7_TRIANGULO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_7_triangulo;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_7_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double BASE, ALTURA, AREA;
+        BASE=10;
+        ALTURA=20;
+        AREA= (BASE*ALTURA)/2;
+        System.out.println(" El area de un TRIANGULO ES");
+        System.out.println(AREA);
+    }
+    
+}
