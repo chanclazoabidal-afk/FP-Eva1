d94ce5a45d6a1e0f6a4db1604e9aacbb82829537
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-09-01 22:23:19 -0700

    Add files via upload

---
+++ EVA_1_8_TEMPERATURA/src/eva_1_8_temperatura/EVA_1_8_TEMPERATURA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_8_temperatura;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double F, CELSIUS ;
+        F=90;
+        CELSIUS= (5*F-32)/9;
+        System.out.println(" TEMPERATURA EN CELSIUS= ");
+        System.out.println(CELSIUS);}}
