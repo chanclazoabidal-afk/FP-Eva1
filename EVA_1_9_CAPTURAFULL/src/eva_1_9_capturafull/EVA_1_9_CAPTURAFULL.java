fae6815378f36013060ad7a46f3c684c84b946dd
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-09-05 19:26:50 -0700

    Add files via upload

---
+++ EVA_1_9_CAPTURAFULL/src/eva_1_9_capturafull/EVA_1_9_CAPTURAFULL.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_9_capturafull;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_9_CAPTURAFULL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
