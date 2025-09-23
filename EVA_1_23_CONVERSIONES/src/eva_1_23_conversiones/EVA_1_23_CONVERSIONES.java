da6b67da7a08aceaeeba3fa41eac447320db4e99
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-09-22 23:24:39 -0700

    Add files via upload

---
+++ EVA_1_23_CONVERSIONES/src/eva_1_23_conversiones/EVA_1_23_CONVERSIONES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_23_conversiones;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA_1_23_CONVERSIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int temp;
+         double tempDecimal;
+         tempDecimal=24.5;
+         temp=(int)tempDecimal;
+         System.out.println(temp);
+         String cade, mensaje;
+         mensaje="LA TEMPERATURA ES";
+         cade=mensaje+temp;//concatenacion
+         System.out.println(cade); 
+         
+                 
+    }
+    
+}
